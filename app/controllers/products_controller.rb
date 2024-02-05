6e35d434a77523f9e9c1003c4409ed0a5a47fe5e
Jayeon Hwang <jayeon512@gmail.com>
2024-02-04 18:54:03 -0600

    add update and destroy actions for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,33 +6,38 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id:params[:id])
     render :show
   end
 
   def create
     @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
       supplier_id: params[:supplier_id],
-      name = params[:name],
-      price = params[:price],
-      description: params[:description]
+    )
+    @product.save
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id:params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      description: params[:description] || @product.description,
+      supplier_id: params[:supplier_id] || @product.supplier_id,
     )
 
-    if @product.save
-      Image.create(
-        product_id: @product.id,
-        url: response.parse["output"][0]
-      )
-    end
-    
-  end
-  
-  
+    render :show
   end
 
-
-
-  
+  def destroy
+    @product = Product.find_by(id:params[:id])
+    @product.destroy
+    render json:{ message: "Product destroyed successfully"}
+  end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
+
 end
