00531237c0e7d332f9480667cd5ea59a5230166c
Jayeon Hwang <jayeon512@gmail.com>
2024-03-12 14:48:24 -0500

    add actions for shoppingcarts

--- app/views/carted_products/_carted_product.json.jbuilder
+++ app/views/carted_products/_carted_product.json.jbuilder
@@ -1,2 +1,6 @@
-json.extract! carted_product, :id, :created_at, :updated_at
-json.url carted_product_url(carted_product, format: :json)
+json.id carted_product.id
+json.user_id carted_product.user_id
+json.order_id carted_product.order_id
+json.product carted_product.product
+json.quantity carted_product.quantity
+json.status carted_product.status
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,12 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
 
+  get "/orders" => "orders#index"
+  get "/orders/:id" => "orders#show" 
+  post "/orders" => "orders#create" 
+
+  get "/carted_products" => "carted_products#index"
+  post "/carted_products" => "carted_products#create"
+  delete "/carted_products/:id" => "carted_products#destroy"
+  
 end
