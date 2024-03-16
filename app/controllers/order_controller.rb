f818b626c53f8395a7cd781f11869e0dfb38359b
Jayeon Hwang <jayeon512@gmail.com>
2024-03-16 14:13:36 -0500

    add order actions

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,44 @@
 class OrderController < ApplicationController
+  before_action :authenticate_user
+  
+  def index  
+    @orders = current_user.orders
+    render :index    
+  end
+  
+  def show
+    @order = Order.find_by(id: params[:id])    
+    if current_user.id == @order.user_id
+      render :show
+    else
+      render json: {message: "you can't do this"}
+    end
+  end
+  
+  def create	
+    @carted_products = CartedProduct.where(status: "carted", user_id: current_user.id)
+    calculated_subtotal = 0
+    @carted_products.each do |carted_product|      
+      calculated_subtotal += carted_product.quantity * carted_product.product.price      
+    end   
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_tax + calculated_subtotal
+
+    @order = Order.new(
+      user_id: current_user.id,
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total 
+    )
+    @order.save
+
+    @carted_products = CartedProduct.where(status: "carted", user_id: current_user.id)
+    @carted_products.each do |carted_product|
+      carted_product.status = 'purchased'
+      carted_product.order_id = @order.id
+      carted_product.save
+    end
+    render :show
+  end
+
 end
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,2 +1,13 @@
-json.extract! order, :id, :created_at, :updated_at
-json.url order_url(order, format: :json)
+json.id order.id
+json.subtotal order.subtotal
+json.tax order.tax
+json.total order.total
+json.created_at order.created_at
+json.updated_at order.updated_at
+json.carted_products order.carted_products.map do |carted_product|
+  json.id carted_product.id
+  json.quantity carted_product.quantity
+  json.status carted_product.status
+  json.product carted_product.product  
+  json.product_images carted_product.product.images  
+end
